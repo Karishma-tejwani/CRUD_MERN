d3b8bd09e15f1f3723224df71332c33073ccf767
Karishma-tejwani <karishmatejwani9@gmail.com>
2022-07-17 17:54:37 +0500

    model added

--- server/index.js
+++ server/index.js
@@ -2,10 +2,23 @@ const express = require('express');
 const mongoose = require("mongoose");
 const app = express();
 
+const foodModel = require("./models/Food");
+
 app.use(express.json());
 
-mongoose.connect()
+mongoose.connect("mongodb://127.0.0.1:27017/food");
+
+app.get("/", async(req, res) => {
+    const food = new foodModel({foodName: "Apple", daySinceIAte: 3});
+    try{
+        await food.save();
+        res.send("data inserted");
+    }
+    catch(err){
+        console.log(err);
+    }
+});
 
 app.listen(3001, () => {
     console.log("Server is listening at port 3001");
-})
+});
